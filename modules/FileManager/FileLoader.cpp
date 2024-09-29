f40de1aa4ffe195f3710bc07d34c6970a2cd7f07
OribitCpp <1508859213@qq.com>
2024-09-29 20:48:23 +0800

    file loader

--- modules/FileManager/FileLoader.cpp
+++ modules/FileManager/FileLoader.cpp
@@ -1,6 +1,14 @@
 #include "FileLoader.h"
+#include "Logger.h"
+#include <llvm/BinaryFormat/Magic.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Object/ObjectFile.h>
+#include <llvm/Object/Archive.h>
 
-FileLoader::FileLoader()
+using namespace llvm;
+
+FileLoader::FileLoader(const std::string& fileName, llvm::LLVMContext& context):m_fileName(fileName),m_context(context)
 {
 }
 
@@ -8,7 +16,48 @@ FileLoader::~FileLoader()
 {
 }
 
-std::string FileLoader::loadAsString()
+void FileLoader::load(std::vector<std::unique_ptr<llvm::Module>>& result)
 {
-	return std::string();
+	ErrorOr<std::unique_ptr<MemoryBuffer>> bufferErr = MemoryBuffer::getFileOrSTDIN(m_fileName);
+	if (bufferErr.getError()) {
+		Logger::error("load file {} failed", m_fileName);
+		return;
+	}
+
+	MemoryBufferRef buffer = bufferErr.get()->getMemBufferRef();
+	file_magic magic = identify_magic(buffer.getBuffer());
+	switch (magic)
+	{
+	case file_magic::bitcode: {
+		SMDiagnostic err;
+		result.emplace_back(parseIR(buffer, err, m_context));
+		return;
+	}
+
+	case file_magic::archive: {
+		loadArchive(buffer, result);
+		return;
+	}
+	default:
+		break;
+	}
+}
+
+void FileLoader::loadArchive(MemoryBufferRef &buffer, std::vector<std::unique_ptr<llvm::Module>>& result)
+{
+	Expected<std::unique_ptr<object::Binary>> archOwner = object::createBinary(buffer, &m_context);
+	if (!archOwner) {
+		std::error_code ec = errorToErrorCode(archOwner.takeError());
+		Logger::error("load file {} failed, {}", m_fileName, ec.message().c_str());
+		return;
+	}
+	object::Binary* arch = archOwner.get().get();
+	object::Archive* archive = dyn_cast<object::Archive>(arch);
+	if (archive) {
+		auto Err = Error::success();
+		for (auto AI = archive->child_begin(Err); AI != archive->child_end(); ++AI)
+		{
+
+		}
+	}
 }
--- modules/FileManager/FileLoader.h
+++ modules/FileManager/FileLoader.h
@@ -3,12 +3,24 @@
 
 #include <filesystem>
 #include <string>
+#include <memory>
+#include <vector>
+
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/Support/MemoryBuffer.h>
 
 class FileLoader {
 public:
-	FileLoader();
+	FileLoader(const std::string& fileName, llvm::LLVMContext& context);
 	~FileLoader();
-	std::string loadAsString();
+
+	 void load(std::vector<std::unique_ptr<llvm::Module>> &result);
+private:
+	void loadArchive(llvm::MemoryBufferRef &buffer,std::vector<std::unique_ptr<llvm::Module>> &result);
+private:
+	std::string m_fileName;
+	llvm::LLVMContext& m_context
 };
 
 #endif // !FILE_LOADER_H
